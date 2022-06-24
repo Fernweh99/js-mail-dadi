17d5789eed1d6d4e33e6f18fa9856893a91a0f17
Fernweh99 <daniele.dimento@gmail.com>
2022-06-24 15:50:27 +0200

    finish all bonus dadi

--- bonus-dadi/js-dadi/script.js
+++ bonus-dadi/js-dadi/script.js
@@ -5,23 +5,36 @@ punteggio più alto e stampa  in console il risultato
 (ossia se ha vinto il computer o il player)
  */
 
-//Dichiaro 2 costanti con numeri random da 1 a 6
-const randomNumberPlayer = (Math.floor(Math.random() * 6)) + 1;
-const randomNumberComputer = (Math.floor(Math.random() * 6)) + 1;
+// Recupero gli elementi dal DOM
+// bottoni
+const buttonPlay = document.getElementById("play");
+// Paragrafi
+const phNumberPlayer = document.getElementById("number-player");
+const phNumberComputer = document.getElementById("number-computer");
+const phGameResult = document.getElementById("game-result");
 
-//Creo una variabile già definita nel caso in cui il giocatore non dovesse vincere o pareggiare
-let result = "Ha vinto il computer!";
+buttonPlay.addEventListener("click", function(){
 
-//Eseguo un controllo su chi ha vinto e se necessario cambio il risultato
-if (randomNumberPlayer > randomNumberComputer) {
-  result = "Ha vinto il giocatore!";
-}
-else if (randomNumberPlayer === randomNumberComputer) {
-  result = "Avete pareggiato!";
-}
+  //Dichiaro 2 costanti con numeri random da 1 a 6
+  const randomNumberPlayer = (Math.floor(Math.random() * 6)) + 1;
+  const randomNumberComputer = (Math.floor(Math.random() * 6)) + 1;
+  
+  //Creo una variabile già definita nel caso in cui il giocatore non dovesse vincere o pareggiare
+  let result = "Ha vinto il computer!";
+  
+  //Eseguo un controllo su chi ha vinto e se necessario cambio il risultato
+  if (randomNumberPlayer > randomNumberComputer) {
+    result = "Ha vinto il giocatore!";
+  }
+  else if (randomNumberPlayer === randomNumberComputer) {
+    result = "Avete pareggiato!";
+  }
+  
+  //Stampo i numeri del giocatore e del computer e mando il risultato su finestra
+  phNumberPlayer.innerText = randomNumberPlayer;
+  phNumberComputer.innerText = randomNumberComputer;
+  phGameResult.innerText = result;
 
-//Stampo i numeri del giocatore e del computer e mando il risultato su finestra
-console.log(`Il numero del giocatore ${randomNumberPlayer}`)
-console.log(`Il numero del computer ${randomNumberComputer}`)
-alert(result);
-
+  
+})
+  
