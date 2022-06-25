0a6fd6f92c6f9d5aa865448dbdb50d6994ede338
Fernweh99 <daniele.dimento@gmail.com>
2022-06-25 21:53:51 +0200

    other extra bonus

--- bonus-dadi/js-dadi/script.js
+++ bonus-dadi/js-dadi/script.js
@@ -12,29 +12,57 @@ const buttonPlay = document.getElementById("play");
 const divNumberPlayer = document.getElementById("number-player");
 const divNumberComputer = document.getElementById("number-computer");
 const phGameResult = document.getElementById("game-result");
+const phWinsPlayer = document.getElementById("wins-player");
+const phWinsComputer = document.getElementById("wins-computer");
 
+let winsPlayer = 0;
+let winsComputer = 0;
+let targetToWin = "";
 buttonPlay.addEventListener("click", function(){
-
+  
   //Dichiaro 2 costanti con numeri random da 1 a 6
   const randomNumberPlayer = (Math.floor(Math.random() * 6)) + 1;
   const randomNumberComputer = (Math.floor(Math.random() * 6)) + 1;
-  
+  buttonPlay.innerText = "GIOCA";
+
+  if (targetToWin == "") {
+    targetToWin = prompt("Decidi il punteggio a cui arrivare per terminare la partita");
+  }
+
   //Creo una variabile già definita nel caso in cui il giocatore non dovesse vincere o pareggiare
   let result = "Ha vinto il computer!";
-
+  
   //Eseguo un controllo su chi ha vinto e se necessario cambio il risultato
   if (randomNumberPlayer > randomNumberComputer) {
     result = "Ha vinto il giocatore!";
-    
+    winsPlayer += 1;
   }
   else if (randomNumberPlayer === randomNumberComputer) {
     result = "Avete pareggiato!";
+  }
+  else {
+    winsComputer += 1;
   }
 
   //Stampo i numeri del giocatore e del computer e mando il risultato su finestra
   divNumberPlayer.innerHTML = `<img src="img/d${randomNumberPlayer}.gif" alt="dado">`;
   divNumberComputer.innerHTML = `<img src="img/d${randomNumberComputer}.gif" alt="dado">`;
-  phGameResult.innerText = result;
+  phWinsPlayer.innerText = winsPlayer;
+  phWinsComputer.innerText = winsComputer;
+  phGameResult.innerHTML = result;
   
+  if ((winsComputer == targetToWin) || (winsPlayer == targetToWin)) {
+    if (winsComputer > winsPlayer) {
+      result = "IL VINCITORE DELLA PARTITA E' IL COMPUTER !"
+    }
+    else {
+      result = "IL VINCITORE DELLA PARTITA E' IL PLAYER !"
+    }
+    winsComputer = 0;
+    winsPlayer = 0;
+    targetToWin = "";
+    phGameResult.innerHTML = `<h3>${result}</h3>`;
+    buttonPlay.innerText = "RIGIOCA";
+  }
 })
   
